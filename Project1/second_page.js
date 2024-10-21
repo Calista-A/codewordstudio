85401166550fa5a5fae70118eb3e8fd014679d19
Calista Ang <s3995320@student.rmit.edu.au>
2024-10-22 01:38:23 +1100

    Update second_page.js

--- Project1/second_page.js
+++ Project1/second_page.js
@@ -1,10 +1,14 @@
+var bgSound;
 var secondPage = function(p) {
     let xPos, yPos;
     let secondPageGif, arrowButton;
 
     p.preload = function() {
         secondPageGif = p.createImg('./data/IMG_2004.GIF');
-    }
+         if (!bgSound) {
+            bgSound = p.loadSound('./data/bg.wav');
+        }
+    };
 
     p.setup = function() {
         p.createCanvas(p.windowWidth, p.windowHeight);
@@ -17,6 +21,25 @@ var secondPage = function(p) {
         // Setup GIF
         secondPageGif.position(xPos + p.width / 3.7, yPos - p.height / 5.2);
         secondPageGif.size(p.width / 5, p.height / 4);
+        
+         // Play/Stop button to control the background sound
+        playStopButton = p.createButton('Play Sound');
+        playStopButton.style('font-size', '15px');
+        playStopButton.style('cursor', 'pointer');
+        playStopButton.style('background-color', 'grey');  
+        playStopButton.style('border', 'none');  // Remove button border
+        playStopButton.position(20, p.height - 40);  // Bottom left corner
+
+        // Toggle play/stop functionality
+        playStopButton.mousePressed(function() {
+            if (bgSound.isPlaying()) {
+                bgSound.stop();  // Stop the sound
+                playStopButton.html('Play Sound');  // Update button text
+            } else {
+                bgSound.loop();  // Play the sound
+                playStopButton.html('Stop Sound');  // Update button text
+            }
+        });
 
         // Arrow button to switch to the third page
         arrowButton = p.createButton('→');
@@ -46,11 +69,9 @@ var secondPage = function(p) {
         p.remove();  // Remove the sketch
         secondPageGif.remove();  // Remove elements related to this sketch
         arrowButton.remove();
+        playStopButton.remove();
     }
 }
-
-
-
 
 //whole code was copied from chat gpt for debugging
 //with own changes for the textSize, Gif & arrowButton and copied function and setup from Project1.js file
